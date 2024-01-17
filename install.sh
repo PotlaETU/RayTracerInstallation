bd128946936af91776691caebab1789d71ab99f5
Antho <anthony.rolland@proton.me>
2024-01-17 19:39:53 +0100

    :sparkles: Le nom de domaine est configuré

--- install.sh
+++ install.sh
@@ -9,6 +9,15 @@
 # local : 192.168.2.1
 # dmz : 10.0.2.1
 
+#nom des machines :
+#routeur : sotoca
+#serveur : samba
+#client1 : danso
+#client2 : wahi
+#autre : medina
+
+domaine=raytracing.fr
+
 case $1 in
   routeur)
       echo "Configuration du routeur . . ."
@@ -18,30 +27,38 @@ case $1 in
       sysctl net.ipv4.conf.all.forwarding=1
       tar xvPpzf fichiers-routeur.tar.gz
       systemctl restart NetworkManager
+      newHostname=sotoca
   ;;
   serveur)
       echo "Configuration du serveur . . ."
       nmcli con mod eth0 autoconnect true ipv4.method auto
       systemctl restart NetworkManager
+      newHostname=samba
   ;;
   client1)
       echo "Configuration du client1 . . ."
       nmcli con mod eth0 autoconnect true ipv4.method auto
       systemctl restart NetworkManager
+      newHostname=danso
   ;;
   client2)
       echo "Configuration du client2 . . ."
       nmcli con mod eth0 autoconnect true ipv4.method auto
       systemctl restart NetworkManager
+      newHostname=wahi
 
   ;;
   autre)
       echo "Configuration de la machine de l'Internet . . ."
       nmcli con mod eth0 autoconnect true ipv4.method auto
       systemctl restart NetworkManager
+      newHostname=medina
   ;;
   *)
 	echo "Mauvais nom de machine ! Veuillez réssayer"
 	exit 1
     ;;
 esac
+
+echo "$newHostname.$domaine" > /etc/hostname
+hostname "$newHostname.$domaine"
