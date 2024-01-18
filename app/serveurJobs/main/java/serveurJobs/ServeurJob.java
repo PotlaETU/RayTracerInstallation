771ea2fc8191e7812731971da30613e806b2f3ed
RayzerDev <rayzergamer62@outlook.fr>
2024-01-18 19:21:23 +0100

    Change la structure des programmes

---
+++ app/serveurJobs/main/java/serveurJobs/ServeurJob.java
@@ -0,0 +1,188 @@
+package serveurJobs;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ServeurJob {
+
+    private static final String imagesPath = "/var/rayTracer/images/";
+
+    private static final ByteBuffer bbSize = ByteBuffer.allocate(4);
+
+    private static final int PORT = 5000;
+    private static final List<byte[]> scenesToGenerate = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress(PORT));
+
+            System.out.println("Serveur de job à l'écoute sur le port " + PORT);
+
+            while (true) {
+                SocketChannel clientChannel = serverSocketChannel.accept();
+                new Thread(() -> {
+                    try {
+                        handleClient(clientChannel);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static synchronized String receiveMessage(SocketChannel channel) throws IOException {
+        channel.read(bbSize);
+        bbSize.flip();
+        int typeSize = bbSize.getInt();
+        bbSize.clear();
+
+        ByteBuffer buffer = ByteBuffer.allocate(typeSize);
+        channel.read(buffer);
+        return new String(buffer.array(), StandardCharsets.UTF_8);
+    }
+
+    private static synchronized void sendMessage(SocketChannel channel, String msg) throws IOException {
+        byte[] msgBytes = msg.getBytes(StandardCharsets.UTF_8);
+
+        ByteBuffer bbSize = ByteBuffer.allocate(4)
+                .putInt(msgBytes.length)
+                .flip();
+        channel.write(bbSize);
+        channel.write(ByteBuffer.wrap(msgBytes));
+    }
+
+    private static void handleClient(SocketChannel clientChannel) throws IOException {
+        try {
+            String requestType = receiveMessage(clientChannel);
+            switch (requestType) {
+                case "ENQUEUEJOB" -> {
+                    sendMessage(clientChannel,"ENQUEUEJOB-ACK");
+
+                    clientChannel.read(bbSize);
+                    bbSize.flip();
+                    int sceneDataLength = bbSize.getInt();
+                    bbSize.clear();
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+                    while (baos.size() < sceneDataLength) {
+                        clientChannel.read(buffer);
+                        buffer.flip();
+                        byte[] bytes = new byte[buffer.remaining()];
+                        buffer.get(bytes);
+                        baos.write(bytes);
+                        buffer.clear();
+                    }
+
+                    scenesToGenerate.add(baos.toByteArray());
+
+                    System.out.println("ENQUEUEJOB: Scene ajoutée à la liste.");
+
+                    sendMessage(clientChannel,"ENQUEUEJOB-OK");
+                }
+                case "REQUESTJOB" -> {
+                    sendMessage(clientChannel,"REQUESTJOB-ACK");
+                    if (!scenesToGenerate.isEmpty()) {
+                        sendMessage(clientChannel, "REQUESTJOB-OK");
+
+                        byte[] nextSceneData = scenesToGenerate.remove(0);
+
+                        bbSize.putInt(nextSceneData.length);
+                        bbSize.flip();
+                        clientChannel.write(bbSize);
+                        bbSize.clear();
+
+                        clientChannel.write(ByteBuffer.wrap(nextSceneData));
+
+                        System.out.println("SENDJOB: Scène envoyé.");
+
+                        String status = receiveMessage(clientChannel);
+
+                        if("SAVEIMAGE".equals(status)){
+                            sendMessage(clientChannel, "SAVEIMAGE-ACK");
+                            System.out.println("SAVEIMAGE: Demande d'enregistrement d'image");
+
+                            ByteBuffer imageSizeBuffer = ByteBuffer.allocate(4);
+                            clientChannel.read(imageSizeBuffer);
+                            imageSizeBuffer.flip();
+                            int imageSize = imageSizeBuffer.getInt();
+                            imageSizeBuffer.clear();
+
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+                            while (baos.size() < imageSize) {
+                                clientChannel.read(buffer);
+                                buffer.flip();
+                                byte[] bytes = new byte[buffer.remaining()];
+                                buffer.get(bytes);
+                                baos.write(bytes);
+                                buffer.clear();
+                            }
+
+                            byte[] imageBytes = baos.toByteArray();
+                            ByteArrayInputStream bais = new ByteArrayInputStream(imageBytes);
+
+                            BufferedImage bi = ImageIO.read(bais);
+
+                            bais.close();
+                            baos.close();
+                            File imagesDirectory = new File(imagesPath);
+
+                            if (!imagesDirectory.exists()) {
+                                if (imagesDirectory.mkdirs()) {
+                                    System.out.println("Répertoire créé avec succès !");
+                                } else {
+                                    System.err.println("Erreur lors de la création du répertoire.");
+                                }
+                            }
+                            ImageIO.write(bi, "png", new File(imagesPath + UUID.randomUUID() + ".png"));
+
+
+                            System.out.println("SAVEIMAGE: Image reçue et enregistrée.");
+                            sendMessage(clientChannel, "SAVEIMAGE-OK");
+
+                            clientChannel.close();
+
+                        }else{
+                            sendMessage(clientChannel,"SAVEIMAGE-ERROR");
+                            System.out.println("SAVEIMAGE: Image non reçue");
+                        }
+
+                    } else {
+                        sendMessage(clientChannel, "REQUESTJOB-NOSCENE");
+                        System.out.println("REQUESTJOB: Pas de scène à générer.");
+                    }
+                }
+
+                default -> System.out.println("Requête inconnue: " + requestType);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            clientChannel.close();
+        }
+    }
+}
---
+++ app/soumissionJob/main/java/soumissionJob/SoumissionJob.java
@@ -0,0 +1,98 @@
+package soumissionJob;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SoumissionJob {
+
+    private static final ByteBuffer bbSize = ByteBuffer.allocate(4);
+
+    private static String receiveMessage(SocketChannel channel) throws IOException {
+        channel.read(bbSize);
+        bbSize.flip();
+        int typeSize = bbSize.getInt();
+        bbSize.clear();
+
+        ByteBuffer buffer = ByteBuffer.allocate(typeSize);
+        channel.read(buffer);
+        return new String(buffer.array(), StandardCharsets.UTF_8);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0){
+            System.err.println("java -jar app.jar <fichier scene>");
+        }
+        String serverAddress = "localhost";
+        int serverPort = 5000;
+
+        SocketChannel server = SocketChannel.open();
+        SocketAddress socketAddr = new InetSocketAddress(serverAddress, serverPort);
+        try {
+            server.connect(socketAddr);
+        }
+        catch (ConnectException e){
+            System.err.println("Le serveur ne répond pas.");
+            System.err.println("La scène n'a pas était soumise.");
+            System.exit(1);
+        }
+
+        Path path = Paths.get(args[0]);
+        byte[] sceneBytes = Files.readAllBytes(path);
+
+        byte[] msgBytes = "ENQUEUEJOB".getBytes(StandardCharsets.UTF_8);
+
+        bbSize.putInt(msgBytes.length)
+                .flip();
+        server.write(bbSize);
+        bbSize.clear();
+        server.write(ByteBuffer.wrap(msgBytes));
+
+        System.out.println("Demande de job transmise.");
+
+        if("ENQUEUEJOB-ACK".equals(receiveMessage(server))){
+            System.out.println("Le serveur est prêt à recevoir la scène");
+        }
+
+        bbSize.putInt(sceneBytes.length)
+                .flip();
+
+        server.write(bbSize);
+        bbSize.clear();
+
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        int position = 0;
+        while (position < sceneBytes.length) {
+            int remaining = sceneBytes.length - position;
+            int bytesToWrite = Math.min(remaining, buffer.capacity());
+
+            buffer.put(sceneBytes, position, bytesToWrite);
+            buffer.flip();
+
+            server.write(buffer);
+
+            buffer.clear();
+            position += bytesToWrite;
+        }
+
+        if("ENQUEUEJOB-OK".equals(receiveMessage(server))){
+            System.out.println("Le serveur a enregistré la scène");
+        }
+        else{
+            System.out.println("Le serveur n'a pas enregistré la scène");
+        }
+
+        System.out.println("Fin du programme.");
+
+        server.close();
+    }
+}
